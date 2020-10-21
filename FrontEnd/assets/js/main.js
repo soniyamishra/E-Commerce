6b4b18ef54ce4ff0be5f8fa695c92f34dc381e52
Keshav Mishra <keshav.sm@somaiya.edu>
2020-10-21 20:03:38 +0530

    update ui

--- FrontEnd/assets/js/main.js
+++ FrontEnd/assets/js/main.js
@@ -1,3 +1,12 @@
 const date = new Date();
 document.querySelector('.year').innerHTML = date.getFullYear();
 
+const plusMinusButton = document.getElementById('plus-minus-button');
+const plusMinusIcon = document.getElementById('plus-minus');
+plusMinusButton.addEventListener('click', ()=>{
+  if(plusMinusIcon.className == 'fas fa-plus'){
+    plusMinusIcon.setAttribute('class', 'fas fa-minus');
+  }else{
+    plusMinusIcon.setAttribute('class', 'fas fa-plus');
+  }
+});
