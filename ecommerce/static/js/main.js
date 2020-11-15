8dc98a48103575844ffb4794229293c87531fe91
mishrakeshav <keshav.sm@somaiya.edu>
2020-11-15 12:06:08 +0530

    update

--- ecommerce/static/js/main.js
+++ ecommerce/static/js/main.js
@@ -14,3 +14,24 @@ plusMinusButton.addEventListener('click', ()=>{
 $(function(){
   $('#demo').multiselect();
 });
+
+
+function sticky_relocate() {
+  var window_top = $(window).scrollTop();
+  var footer_top = $("#footer").offset().top;
+  var div_top = $('#sticky-anchor').offset().top;
+  var div_height = $("#sticky").height();
+  
+  if (window_top + div_height > footer_top)
+      $('#sticky').removeClass('stick');    
+  else if (window_top > div_top) {
+      $('#sticky').addClass('stick');
+  } else {
+      $('#sticky').removeClass('stick');
+  }
+}
+
+$(function () {
+  $(window).scroll(sticky_relocate);
+  sticky_relocate();
+});
